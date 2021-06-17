9740794dcecf978fe385849400d299e59003c4f1
okabedev <okabedev@gmail.com>
2021-06-17 17:46:32 +0100

    update input

--- run.py
+++ run.py
@@ -33,22 +33,24 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--process', default=4, dest="process", type=int)
     parser.add_argument('--obj_per_img', default=20, dest="obj_per_img", type=int)
-    parser.add_argument('--save', required=True, dest="save", type=str)
-    parser.add_argument('--count', required=True, dest="count", type=int)
-    parser.add_argument('--test_percent', required=True, dest="test_percent", type=int)
+    parser.add_argument('--root', required=True, dest="root", type=str)
+    parser.add_argument('--dataset_name', default="train", dest="dataset_name", type=str)
+    parser.add_argument('--start', default=0, dest="start", type=int)
+    parser.add_argument('--end', required=True, dest="end", type=int)
     args, _ = parser.parse_known_args()
 
-    max_zeros = len(str(args.count - 1))
-    jobs = list(map(lambda x: str(x).zfill(max_zeros), range(args.count)))
+    end_num = args.end
+    max_zeros = len(str(end_num))
+    jobs = list(map(lambda x: str(x).zfill(max_zeros), range(args.start, end_num + 1)))
     random.shuffle(jobs)
 
-    train_count = int(round((100 - args.test_percent) / 100 * args.count))
     nb_process = args.process
-    run_sub(nb_process, jobs[:train_count], executable, os.path.join(args.save, "train"), args)
-    run_sub(nb_process, jobs[train_count:], executable, os.path.join(args.save, "test"), args)
 
+    root_path = args.root
+    if not os.path.isdir(root_path):
+        os.makedirs(root_path)
 
-def run_sub(nb_process: int, jobs: list, executable: str, save_path: str, args):
+    save_path = os.path.join(root_path, args.dataset_name)
     if not os.path.isdir(save_path):
         os.makedirs(save_path)
 
@@ -59,7 +61,7 @@ def run_sub(nb_process: int, jobs: list, executable: str, save_path: str, args):
     job_groups = [jobs[i:i + n] for i in range(0, len(jobs), n)]
 
     with Pool(nb_process) as p:
-        func = partial(handle, executable, args.save, save_path, args.obj_per_img)
+        func = partial(handle, executable, root_path, save_path, args.obj_per_img)
         p.map(func, job_groups)
 
 
